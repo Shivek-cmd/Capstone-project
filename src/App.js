f59c802b82ff9cfbceb25e9a20e84a8052df2255
Shivek Soni <shivek6856@gmail.com>
2024-06-03 19:28:25 +0530

    Page 4 Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./pages/Form/Form.jsx";
 import Genre from "./pages/Genre/Genre.jsx";
 import Info from "./pages/Info/Info.jsx";
-import Movies from "./pages/Movies";
+import Movies from "./pages/Movies/Movies.jsx";
 import NotFound from "./pages/404";
 
 function App() {
